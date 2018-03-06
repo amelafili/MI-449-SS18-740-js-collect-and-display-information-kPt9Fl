6a7f8624f97f66f35b9e60fe1ca5adb3a48e555c
amelafili <31477792+amelafili@users.noreply.github.com>
2018-03-05 19:11:30 -0500

    different js process

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 var firstName = document.getElementById('first-name')
-firstName.onkeyup = function () {
-  document.getElementById('first1').innerHTML = firstName.value
-}
-
 var lastName = document.getElementById('last-name')
-lastName.onkeyup = function () {
-  lastName = document.getElementById('last1').innerHTML = lastName.value
+var emailInp = document.getElementById('email')
+var phoneInp = document.getElementById('tel')
+var desInp = document.getElementById('description')
+var raw = document.getElementById('rawID')
+var preview = document.getElementById('preview')
+
+function updateprofile () {
+  var first = document.getElementById('live-update').innerHTML = firstName.value
+  var last = lastName.value
+  var email = emailInp.value
+  var tel = phoneInp.value
+  var desc = desInp.value
+  var result = 'Hi, my name is' + first + last + 'If youre interested in a date, you can email me at' + email + 'or give me a call at' + tel
+  return result
 }
 
-var email = document.getElementById('email')
-email.onkeyup = function () {
-  document.getElementById('email1').innerHTML = email.value
-}
+firstName.addEventListener('input', updateprofile())
 
-var phone = document.getElementById('tel')
-phone.onkeyup = function () {
-  document.getElementById('phone1').innerHTML = phone.value
-}
+lastName.addEventListener('input', updateprofile())
 
-var descr = document.getElementById('description')
-descr.onkeyup = function () {
-  document.getElementById('descrip').innerHTML = descr.value
+emailInp.addEventListener('input', updateprofile())
+
+phoneInp.addEventListener('input', updateprofile())
+
+phoneInp.addEventListener('input', updateprofile())
+
+document.getElementById('profile')
+
+function finalpreview () {
+  document.getElementById('first-name').innerHTML = firstName.value
+  document.getElementById('last1').innerHTML = lastName.value
 }
