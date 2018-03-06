780969359ed672ccc427fdbc717fd1b7d411ec71
amelafili <31477792+amelafili@users.noreply.github.com>
2018-03-05 22:00:13 -0500

    added raw HTML

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var lastName = document.getElementById('last-name')
 var description = document.getElementById('description')
 var email = document.getElementById('email')
 var phone = document.getElementById('phone')
-var rawparagraph = document.getElementById('rawID')
+var rawParagraph = document.getElementById('rawID')
 
 firstName.addEventListener('input', function () {
   var first = firstName.value
@@ -32,5 +32,42 @@ email.addEventListener('input', function () {
 phone.addEventListener('input', function () {
   var phone1 = phone.value
   var phoneInput = document.getElementById('phone1')
+  phoneInput.innerHTML = phone1
+})
+
+var rawfirstName = document.getElementById('first-name')
+var rawlastName = document.getElementById('last-name')
+var rawdescription = document.getElementById('description')
+var rawemail = document.getElementById('email')
+var rawphone = document.getElementById('phone')
+var rawParagraph = document.getElementById('rawID')
+
+rawfirstName.addEventListener('input', function () {
+  var first = firstName.value
+  var first1Input = document.getElementById('rawfirst')
+  first1Input.innerHTML = first
+})
+
+rawlastName.addEventListener('input', function () {
+  var last = lastName.value
+  var last1Input = document.getElementById('rawlast')
+  last1Input.innerHTML = last
+})
+
+rawdescription.addEventListener('input', function () {
+  var descr1 = description.value
+  var descr1Input = document.getElementById('rawdescr')
+  descr1Input.innerHTML = descr1
+})
+
+email.addEventListener('input', function () {
+  var email1 = email.value
+  var emailInput = document.getElementById('rawemail')
+  emailInput.innerHTML = email1
+})
+
+phone.addEventListener('input', function () {
+  var phone1 = phone.value
+  var phoneInput = document.getElementById('rawphone')
   phoneInput.innerHTML = phone1
 })
