f4e03cfce1a8e5e30a362a0c1f064a946510c6cc
amelafili <31477792+amelafili@users.noreply.github.com>
2018-03-07 07:16:10 -0500

    Added ahref for email and phone #

--- script.js
+++ script.js
@@ -8,41 +8,21 @@ var preview = document.getElementById('preview')
 
 var updateProfile = function () {
   var first = firstName.value
-  var firstInput = document.getElementById('first1')
-  var firstInput1 = document.getElementById('rawfirst')
-  firstInput.innerHTML = first
-  firstInput1.textContent = first
   var last = lastName.value
-  var lastInput = document.getElementById('last1')
-  lastInput.innerHTML = last
   var description = desInput.value
-  var descrip = document.getElementById('descrip')
-  var descrip2 = document.getElementById('rawdescr')
-  descrip.innerHTML = description
-  descrip2.textContent = description
   var email = emailInput.value
-  var email1 = document.getElementById('email1')
-  var email2 = document.getElementById('rawemail')
-  email1.innerHTML = email
-  email2.textContent = email
   var phone = phoneInput.value
-  var phone1 = document.getElementById('phone1')
-  var phone2 = document.getElementById('rawphone')
-  phone1.innerHTML = phone
-  phone2.textContent = phone
-
-  preview.innerHTML =
-  '<h1> Hi, my name is ' +
-  first + last +
-  description + '</h1><p>If youre interested in a date, you can email me at' +
-  email + 'or give me a call at</p>' +
-  phone
-
-  raw.textContent = '<h1> Hi, my name is' +
-  first + last + description +
-  'If youre interested in a date, you can email me at' +
-  email + 'or give me a call at </h1>' +
-  phone
+  preview.innerHTML = '<h1>Hi, my name is ' +
+    first + ' ' + last + ' </h1>' +
+    '<p>' + description + '</p>' +
+    '<p> If you\'re interested in a date, you can email me at ' +
+    '<a href="mailto:email">' + email + '</a>' + ' or give me a call at ' +
+    '<a href="tel:phone">' + phone + '</a>' + '</p>'
+  raw.textContent = '<h1>Hi, my name is ' +
+    first + ' ' + last + ' </h1>' +
+    '<p>' + description + '</p>' +
+    '<p> If you\'re interested in a date, you can email me at ' + email + ' or give me a call at ' +
+    phone + '</p>'
 }
 
 firstName.addEventListener('input', updateProfile)
