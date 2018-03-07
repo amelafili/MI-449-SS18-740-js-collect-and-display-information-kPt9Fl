6237ef534ac3c99b7889f383226ce1ee83b079f4
amelafili <31477792+amelafili@users.noreply.github.com>
2018-03-07 13:53:59 -0500

    created paragraph variable by putting website text and input variables inside of it

--- script.js
+++ script.js
@@ -6,24 +6,22 @@ var desInput = document.getElementById('description')
 var raw = document.getElementById('rawID')
 var preview = document.getElementById('preview')
 
-var updateProfile = function () {
+function updateProfile () {
   var first = firstName.value
   var last = lastName.value
   var description = desInput.value
   var email = emailInput.value
   var phone = phoneInput.value
-  preview.innerHTML = '<h1>Hi, my name is ' +
-    first + ' ' + last + ' </h1>' +
-    '<p>' + description + '</p>' +
-    '<p> If you\'re interested in a date, you can email me at ' +
-    '<a href="mailto:email">' + email + '</a>' + ' or give me a call at ' +
-    '<a href="tel:phone">' + phone + '</a>' + '</p>'
-  raw.textContent = '<h1>Hi, my name is ' +
-    first + ' ' + last + ' </h1>' +
-    '<p>' + description + '</p>' +
-    '<p> If you\'re interested in a date, you can email me at ' +
-    '<a href="mailto:email">' + email + '</a>' + ' or give me a call at ' +
-    '<a href="tel:phone">' + phone + '</a>' + '</p>'
+  var paragraph =
+  '<h1>Hi, my name is ' +
+  first + ' ' + last + ' </h1>' +
+  '<p>' + description + '</p>' +
+  '<p> If you\'re interested in a date, you can email me at ' +
+  '<a href="mailto:email">' + email + '</a>' +
+  ' or give me a call at ' +
+  '<a href="tel:phone">' + phone + '</a>' + '</p>'
+  preview.innerHTML = paragraph
+  raw.textContent = paragraph
 }
 
 firstName.addEventListener('input', updateProfile)
